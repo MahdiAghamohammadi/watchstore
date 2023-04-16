1faa7ac9de3ae259f923da04b1ae267ccbdbb341
mahdi <aghamohammadi.mehdi15@gmail.com>
2023-04-16 22:18:53 +0330

    first commit

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    @include('admin.layouts.head-tag')
+    @livewireStyles
+    <title>@yield('title', 'قالب مدیریتی')</title>
+</head>
+<body class="small-navigation">
+{{--Naviagtion--}}
+@include('admin.layouts.navigation')
+{{--Header--}}
+@include('admin.layouts.header')
+{{--Content--}}
+@yield('content')
+@include('admin.layouts.scripts')
+@livewireScripts
+</body>
+</html>
