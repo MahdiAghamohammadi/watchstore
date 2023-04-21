8fe1f5ef4dd707fd7a5f64d3157a40b57de0ee96
mahdi <aghamohammadi.mehdi15@gmail.com>
2023-04-21 17:25:13 +0330

    added search with livewire

--- app/Http/Livewire/Admin/Users.php
+++ app/Http/Livewire/Admin/Users.php
@@ -7,9 +7,15 @@ use Livewire\Component;
 
 class Users extends Component
 {
+    public $search;
+
     public function render()
     {
-        $users = User::paginate(15);
+        $users = User::query()
+            ->where('name', 'LIKE', "%{$this->search}%")
+            ->orWhere('email', 'LIKE', "%{$this->search}%")
+            ->orWhere('mobile', 'LIKE', "%{$this->search}%")
+            ->paginate(15);
 
         return view('livewire.admin.users', compact('users'));
     }
