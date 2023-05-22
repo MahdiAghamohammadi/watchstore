668ea9ba24ca9725e7fc6049130f77b757e012f9
mahdi <aghamohammadi.mehdi15@gmail.com>
2023-05-22 20:02:26 +0330

    install swagger and make base controller

---
+++ app/Http/Controllers/Api/V1/BaseController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     version="1.0",
+ *     title="Watch Store Api List"
+ * )
+ * @OA\PathItem(path="/api")
+ */
+class BaseController extends Controller
+{
+    //
+}
